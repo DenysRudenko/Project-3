16f1fd1d2666e9f1a7f981bc6cd763a9f5df7cce
Denys <heartbreathing19951014@gmail.com>
2022-09-30 04:12:16 +0000

    Added match_with_gaps function

--- run.py
+++ run.py
@@ -1,17 +1,20 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
-
-# ------------------------------------------------------------------------------------------
+# ------------------------------------------------------
 # Description:
-# This is the Hangman game, at the start of the program you have 2 options to play.
-# The 1st option is to play with clues(hints) by using "*" to get the clue.
-# The 2nd option is to play the game by default without the clues.
-# After each wrong answer you might see the picture of "HangMan" till it`s fully completed.
+# This is the Hangman game, at the start of the program 
+# you have 2 options to play.
+# The 1st option is to play with clues(hints) by using 
+# "*" to get the clue.
+# The 2nd option is to play the game by default without
+# the clues.
+# After each wrong answer you might see the picture of 
+# "HangMan" till it`s fully completed.
 # If you guess the word you win.
 #
 # (C) 2022 Rudenko Denys, Dublin, Ireland
 # Released for Code Insitute
 # Email: heartbreathing19951014@gmail.com
-# ------------------------------------------------------------------------------------------
+# ------------------------------------------------------
 
 import random  
 import string
@@ -68,3 +71,22 @@ def get_available_letters(letters_guessed):
         if letter not in letters_guessed:
             available_letters.append(letter)
     return "".join(available_letters)
+
+
+def match_with_gaps(my_word, other_word):
+    
+    """
+    
+    """
+
+    new_word = my_word.replace(" ", "")
+    if len(new_word) != len(other_word):
+        return False
+    for i in range(len(other_word)):
+        if new_word[i] == "_":
+            if other_word[i] in new_word:
+                return False
+        else:
+            if new_word[i] != other_word[i]:
+                return False
+    return True
