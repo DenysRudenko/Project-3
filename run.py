d6d685221ee5a70886a8c4b1d10b24fc8aaea40c
Denys <heartbreathing19951014@gmail.com>
2022-09-30 03:48:15 +0000

    Added a function that randomly picking a word from words list array

--- run.py
+++ run.py
@@ -3,8 +3,8 @@
 # ------------------------------------------------------------------------------------------
 # Description:
 # This is the Hangman game, at the start of the program you have 2 options to play.
-# 1st option is to play with clues(hints) by using "*" to get the clue.
-# 2nd option is to play the game by default without the clues.
+# The 1st option is to play with clues(hints) by using "*" to get the clue.
+# The 2nd option is to play the game by default without the clues.
 # After each wrong answer you might see the picture of "HangMan" till it`s fully completed.
 # If you guess the word you win.
 #
@@ -12,3 +12,15 @@
 # Released for Code Insitute
 # Email: heartbreathing19951014@gmail.com
 # ------------------------------------------------------------------------------------------
+
+import random  
+import string
+from show_hangman import txt_to_show
+from load_words import load_words
+
+
+def select_word(wordlist):
+    """
+    Random word from array wordlist
+    """
+    return random.choice(wordlist)
