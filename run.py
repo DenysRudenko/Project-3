a615731504d192166435e35b6fbcd9d83886b2de
Denys <heartbreathing19951014@gmail.com>
2022-09-30 03:19:34 +0000

    Added a description of the project at the top

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# ------------------------------------------------------------------------------------------
+# Description:
+# This is the Hangman game, at the start of the program you have 2 options to play.
+# 1st option is to play with clues(hints) by using "*" to get the clue.
+# 2nd option is to play the game by default without the clues.
+# After each wrong answer you might see the picture of "HangMan" till it`s fully completed.
+# If you guess the word you win.
+#
+# (C) 2022 Rudenko Denys, Dublin, Ireland
+# Released for Code Insitute
+# Email: heartbreathing19951014@gmail.com
+# ------------------------------------------------------------------------------------------
